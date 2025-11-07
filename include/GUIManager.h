e9814615d29ff1ce62bce2f6f6db32bd7bea4838
Deversh <139346576+Deversh1253@users.noreply.github.com>
2025-11-07 19:59:20 +0530

    Update GUIManager.h

--- include/GUIManager.h
+++ include/GUIManager.h
@@ -1,1 +1,11 @@
+#pragma once
+#include <vector>
+#include <string>
+
+class GUIManager {
+public:
+    static int displayMainMenu();
+    static void showRows(const std::vector<std::vector<std::string>>& rows,
+                         const std::vector<std::string>& headers = {});
+};
 
