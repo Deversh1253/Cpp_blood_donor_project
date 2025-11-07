f4785d7dfbf22bdb9c630714c15ab98aab639e85
Deversh <139346576+Deversh1253@users.noreply.github.com>
2025-11-07 19:55:59 +0530

    Update Admin.h

--- include/Admin.h
+++ include/Admin.h
@@ -1,1 +1,10 @@
+#pragma once
+#include "DatabaseManager.h"
+
+class Admin {
+public:
+    void approveDonor(DatabaseManager& db, string phone);
+    void rejectDonor(DatabaseManager& db, string phone);
+    void removeInactiveDonor(DatabaseManager& db);
+};
 
