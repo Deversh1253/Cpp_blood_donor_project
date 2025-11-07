8109852e0a1ade145348bd100bf3c4fca535e209
Deversh <139346576+Deversh1253@users.noreply.github.com>
2025-11-07 22:15:47 +0530

    Update Admin.h

--- include/Admin.h
+++ include/Admin.h
@@ -3,8 +3,8 @@
 
 class Admin {
 public:
-    void approveDonor(DatabaseManager& db, string phone);
-    void rejectDonor(DatabaseManager& db, string phone);
+    void approveDonor(DatabaseManager& db, std::string phone);
+    void rejectDonor(DatabaseManager& db, std::string phone); // (console-based)
+    void rejectDonor(DatabaseManager& db, std::string phone, const std::string& reason); // GUI  with reasoning option
     void removeInactiveDonor(DatabaseManager& db);
 };
-
