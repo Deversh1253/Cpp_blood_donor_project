370dad69be7360f228a33ac8a6258c4eb91c3644
Deversh <139346576+Deversh1253@users.noreply.github.com>
2025-11-07 19:58:50 +0530

    Update Donor.h

--- include/Donor.h
+++ include/Donor.h
@@ -1,1 +1,18 @@
+#pragma once
+#include "DatabaseManager.h"
+
+class Donor {
+private:
+    string name, gender, phone, city, address, bloodGroup;
+    int age;
+    bool available, healthTestStatus, eligibilityStatus, approvalStatus;
+    string lastDonationDate;
+
+public:
+    Donor();
+    void registerDonor(DatabaseManager& db);
+    bool checkEligibility();
+    void updateAvailability(DatabaseManager& db, bool newStatus);
+    void showDonationHistory(DatabaseManager& db);
+};
 
