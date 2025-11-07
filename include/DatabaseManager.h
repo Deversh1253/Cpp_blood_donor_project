7f8987d7f2356d2fc8c37ec612261fe0a212a643
Deversh <139346576+Deversh1253@users.noreply.github.com>
2025-11-07 19:58:22 +0530

    Update DatabaseManager.h

--- include/DatabaseManager.h
+++ include/DatabaseManager.h
@@ -1,1 +1,31 @@
+#pragma once
+#include <mysql/mysql.h>
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class DatabaseManager {
+private:
+    MYSQL* conn;
+    MYSQL_RES* res;
+    MYSQL_ROW row;
+    bool connected;
+
+public:
+    DatabaseManager();
+    ~DatabaseManager();
+
+    bool connectDB(string host = "localhost",
+                   string user = "root",
+                   string pass = "root123",
+                   string db = "blood_life_db",
+                   unsigned int port = 3306);
+    void closeConnection();
+
+    bool executeQuery(const string& query);
+    vector<vector<string>> fetchResults(const string& query);
+
+    bool isConnected() const;
+};
 
