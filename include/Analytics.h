b6879984fad62285feeafe5eb717c291bb158836
Deversh <139346576+Deversh1253@users.noreply.github.com>
2025-11-07 19:57:15 +0530

    Update Analytics.h

--- include/Analytics.h
+++ include/Analytics.h
@@ -1,1 +1,15 @@
+#pragma once
+#include "DatabaseManager.h"
+#include <vector>
+#include <string>
+
+class Analytics {
+public:
+    // Count of searches by blood group (optionally by area)
+    static std::vector<std::vector<std::string>>
+    showDemandByBloodGroup(DatabaseManager& db);
+
+    static std::vector<std::vector<std::string>>
+    showAreaWiseTrends(DatabaseManager& db);
+};
 
